8ad4e30d7900b44d3a6df8c86e2964a53052534e
George Freedland <g.freedland@ventra.ru>
2020-02-06 12:32:57 -0800

    front end for m0 added

---
+++ Milestones/M0/frontend/milestone-one/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>csc 648</p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
