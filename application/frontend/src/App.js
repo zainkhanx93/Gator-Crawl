2c80bb16b612074e4d6ec7c3f3dfd9fd13a76d5e
Niko Cruz <francisnikocruz@gmail.com>
2020-03-16 14:37:04 -0700

    :art: Added search functionality
    Added user search functionality for products

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 import noMatch from './Components/noMatch';
 import Team from './Containers/Team';
 import TeamMember from './Containers/TeamMember';
+import SearchResults from './Containers/SearchResults';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <nav>
+            <Link to="/" className="nav-item">
+              Home
+            </Link>
+            <Link to="/team" className="nav-item">
+              Team
+            </Link>
+          </nav>
           CSC 648, Section 2
           <br />
           Software Engineering, Spring 2020
           <br />
           Team 1
+          <br />
         </header>
         <Switch>
-          <Redirect exact from="/" to="/team" />
+          <Route path="/" exact component={SearchResults} />
           <Route path="/team" exact component={Team} />
-          <Route path='/team/:id' exact component={TeamMember} />
+          <Route path="/team/:id" exact component={TeamMember} />
           <Route component={noMatch} />
         </Switch>
-
       </div>
     );
   }
