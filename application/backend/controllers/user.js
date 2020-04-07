6cab984177a179248be542bd61eff26d13815bcc
HuyPhan <tphan7@mail.sfsu.edu>
2020-04-06 19:06:42 -0700

    add regiter function

--- application/backend/controllers/user.js
+++ application/backend/controllers/user.js
@@ -1,60 +1,107 @@
 const models = require('../models');
 
 const { User } = models;
+//Using bcrypt to hash password
+const bcrypt = require('bcryptjs');
 
 // Create and Save a new user
+//Since we login in with 2 fields email and password
 exports.create = (req, res) => {
-  const { username, password } = req.body;
-  if (!password || !username) {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!password || !email) {
     return res.status(400).send({ message: 'Content cannot be empty' });
   }
-
-  // respond with jwt with id as payload
-  User.create(req.body)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      return res
-        .status(500)
-        .send({ error: err, message: 'error creating user' });
-    });
-};
-
-// Find all users
-exports.findAll = (req, res) => {
-  User.findAll()
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        error: err,
-        message: 'Error occurred while retrieving users',
-      });
-    });
-};
-
-// Delete with specified id
-exports.delete = (req, res) => {
-  const { id } = req.params;
-  User.destroy({
-    where: { id },
-  })
-    .then(num => {
-      if (num === 1) {
-        res.send({
-          message: 'User was deleted successfully!',
-        });
+  // respond with jwt with id as payload (maybe later)
+  User.findOne({ where: { email: req.body.email } })
+    .then(function (user) {
+      if (user) {
+        return res.status(400).send({
+          msg: "User already registers"
+        })
       } else {
-        res.send({
-          message: `User delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
-        });
+        //const hashPassword = await bcrypt.hash(req.body.password, 10); raise error
+        const newUser = {
+          email: req.body.email,
+          password: req.body.password,
+        }
+        //bcrypt hash password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hashPassword) => {
+            if (err) throw err;
+            newUser.password = hashPassword;
+            User.create(newUser)
+            .then(data => {
+              //text
+              res.send(data);
+              //Json object
+              //res.json(data);
+              res.redirect('/api/users/login');
+            })
+            .catch(err => {
+              return res
+                .status(500)
+                .send({ error: err, message: 'error creating user' });
+            });
+          })
+        })
       }
+      
+
     })
-    .catch(err => {
-      res
-        .status(500)
-        .send({ error: err, message: `Could not delete user with id ${id}` });
-    });
-};
+  }
+
+  exports.login = (req, res) => {
+    const { email, password } = req.body;
+    if (!password || !email) {
+      return res.status(400).send({ message: 'Content cannot be empty' });
+    }
+
+    User.create(req.body)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        return res
+          .status(500)
+          .send({ error: err, message: 'error creating user' });
+      });
+  };
+
+  // Find all users
+  exports.findAll = (req, res) => {
+    User.findAll()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          error: err,
+          message: 'Error occurred while retrieving users',
+        });
+      });
+  };
+
+  // Delete with specified id
+  exports.delete = (req, res) => {
+    const { id } = req.params;
+    User.destroy({
+      where: { id },
+    })
+      .then(num => {
+        if (num === 1) {
+          res.send({
+            message: 'User was deleted successfully!',
+          });
+        } else {
+          res.send({
+            message: `User delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+          });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ error: err, message: `Could not delete user with id ${id}` });
+      });
+  };
--- application/backend/routes/user.js
+++ application/backend/routes/user.js
@@ -3,10 +3,28 @@ const users = require('../controllers/user.js');
 
 const router = express.Router();
 
+//Register new User
+//POST request public add new User to route /api/users/
 router.post('/', users.create);
 
+//Log in current User
+//Post request to /api/login
+router.post('/login', users.login);
+
+//Show all registered User
+//Get request public to route /api/users/
 router.get('/', users.findAll);
 
+//Delete User
+//Del request private, giving private user's id
 router.delete('/:id', users.delete);
+
+//upload profile image
+//Get request to route /api/users/image to upload profile image
+//router.post('/image', users.uploadImage);
+
+//show profile image
+//Get request to route /api/users/image to see profile image
+//router.get('/image', users.showImage);
 
 module.exports = router;
