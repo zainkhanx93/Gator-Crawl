a67242ba6529595d04aa0d8fd5fd39f4706597f5
gfreedland <gfreedland@mail.sfsu.edu>
2020-05-08 20:26:58 -0700

    quick fix to login and profile

--- application/frontend/src/Containers/Profile.js
+++ application/frontend/src/Containers/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import gclogo from '../Assets/Images/gclogo.png';
 import MainNavBar from '../Components/Navigation/MainNavBar.js';
+import ProfileNavBar from '../Components/Navigation/ProfileNavBar';
 
 
 import './Profile.css';
