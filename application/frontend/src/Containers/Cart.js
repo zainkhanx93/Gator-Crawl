13b80d276646d412de45839a1929d8f3a2c4335f
Niko Cruz <francisnikocruz@gmail.com>
2020-05-08 18:13:55 -0700

    Revert "Revert "Revert "Delete function"""
    This reverts commit 707c17c953453a0d0c609cc74db96e2299b9fdd9.

--- application/frontend/src/Containers/Cart.js
+++ application/frontend/src/Containers/Cart.js
@@ -7,80 +7,76 @@ import gclogo from '../Assets/Images/nike.jpg';
 import './Cart.css';
 
 class Cart extends React.Component {
+  render() {
+    const { history } = this.props;
 
-   delete = () => {
-     // let product = document.getElementById('Cart-Product');
-     // product.removeChild(product.childNodes[0]);
-     console.log('delete product');
-   }
-   //
-   //  product.removeChild(product.childNodes[0]);
+    const mycart = (
+      <div>
+        <p className="Title"> My Shopping Cart </p>
+        <div className="Shopping-Cart-Window">
 
-   render() {
-     const { history } = this.props;
+          <div className="Product-Window">
 
-     const mycart = (
-       <div>
-         <p className="Title"> My Shopping Cart </p>
-         <div className="Shopping-Cart-Window">
+            <div className="Cart-Product">
+              <img className="Profile-picture" src={gclogo} alt="Logo" />
 
-           <div className="Product-Window">
+              <div className="Cart-Product-Info">
+                <p style={{ fontSize: '25px' }}> <b> Nike Shoes </b></p>
+                <p> Size 9 Roshis </p>
+                <p style={{ fontSize: '25px' }}><b> $ 200.00 </b> </p>
+              </div>
+              <div className="Cart-Buttons">
+                <button className="Button" type="button"> Message  </button>
+                <br />
+                <br />
+                <br />
+                <button className="Button" onClick="Delete()" type="button"> Delete </button>
+              </div>
 
-             <div className="Cart-Product">
-               <img className="Profile-picture" src={gclogo} alt="Logo" />
+            </div>
+          </div>
 
-               <div className="Cart-Product-Info">
-                 <p style={{ fontSize: '25px' }}> <b> Nike Shoes </b></p>
-                 <p> Size 9 Roshis </p>
-                 <p style={{ fontSize: '25px' }}><b> $ 200.00 </b> </p>
-               </div>
-               <div className="Cart-Buttons">
-                 <button className="Button" type="button"> Message  </button>
-                 <br />
-                 <br />
-                 <br />
-                 <button className="Button" onClick={this.delete} type="button"> Delete </button>
-               </div>
+          <div className="Total-Box">
+            <div className="Total-Cart-Info-Left">
+              <p style={{ fontSize: '30px', color: '#662A82' }}><b> Summary </b> </p>
+              <br />
+              <p style={{ fontSize: '25px' }}> <b> Total </b></p>
+              <br />
+            </div>
+            <div className="Total-Cart-Info-Right">
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <p style={{ fontSize: '25px' }}> <b> $200 </b></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 
-             </div>
-           </div>
-
-           <div className="Total-Box">
-             <div className="Total-Cart-Info-Left">
-               <p style={{ fontSize: '30px', color: '#662A82' }}><b> Summary </b> </p>
-               <br />
-               <p style={{ fontSize: '25px' }}> <b> Total </b></p>
-               <br />
-             </div>
-             <div className="Total-Cart-Info-Right">
-               <br />
-               <br />
-               <br />
-               <br />
-               <br />
-               <br />
-               <p style={{ fontSize: '25px' }}> <b> $200 </b></p>
-             </div>
-           </div>
-         </div>
-       </div>
-     );
-
-     return (
-       <div>
-         <MainNavBar history={history} />
-         <div className="Cart-Window">
-           <div className="Cart-Left-Side">
-             <ProfileNavBar />
-           </div>
-           <div className="Cart-Right-Side">
-             {mycart}
-           </div>
-         </div>
-       </div>
-     );
-   }
+    return (
+      <div>
+        <MainNavBar history={history} />
+        <div className="Cart-Window">
+          <div className="Cart-Left-Side">
+            <ProfileNavBar />
+          </div>
+          <div className="Cart-Right-Side">
+            {mycart}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 
 export default Cart;
+
+function Delete() {
+  let product = document.getElementById('Cart-Product');
+  product.removeChild(product.childNodes[0]);
+}
